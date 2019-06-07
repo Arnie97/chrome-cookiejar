ae4b4b2cde11107fd32a8431c963350bc66310d5
Arnie97 <arnie97@gmail.com>
2019-06-08 00:32:50 +0800

    Support Linux v10 cookies

--- chrome_cookiejar/decrypt.py
+++ chrome_cookiejar/decrypt.py
@@ -7,4 +7,15 @@ if sys.platform == 'win32':
         return CryptUnprotectData(cipher_blob)[1]
 
 else:
-    decrypt = NotImplemented
+    from .unix_crypt import aes_cbc_decrypt, get_chrome_safe_storage
+
+    def decrypt(cipher_blob: bytes) -> bytes:
+        if not cipher_blob:
+            return cipher_blob
+        elif cipher_blob.startswith(b'v10'):
+            password = b'peanuts'
+        elif cipher_blob.startswith(b'v11'):
+            password = get_chrome_safe_storage()
+        else:
+            raise ValueError('Unknown cipher type: %s' % cipher_blob)
+        return aes_cbc_decrypt(cipher_blob, password)
---
+++ chrome_cookiejar/unix_crypt.py
@@ -0,0 +1,25 @@
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import PBKDF2
+
+
+def aes_cbc_decrypt(
+    cipher_blob: bytes,
+    password: bytes,
+    salt=b'saltysalt',
+    length=16,
+    iv=16 * b' ',
+    iterations=1,
+) -> bytes:
+    key = PBKDF2(password, salt, length, iterations)
+    cipher = AES.new(key, AES.MODE_CBC, IV=iv)
+
+    # strip off the 'v10' prefix before AES decrypt
+    clear_text = cipher.decrypt(cipher_blob[3:])
+
+    # strip off the padding
+    padding = clear_text[-1] if clear_text else 0
+    return clear_text[:-padding]
+
+
+def get_chrome_safe_storage() -> bytes:
+    raise NotImplementedError
