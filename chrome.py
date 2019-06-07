49d6e99aa1cddcd4f12ea11cdffe1fe52ccb0ee8
Arnie97 <arnie97@gmail.com>
2019-06-07 16:21:18 +0800

    Refactor: extract functions

--- chrome.py
+++ chrome.py
@@ -1,11 +1,12 @@
 import datetime
 import http.cookiejar
 import sqlite3
+from typing import Iterable
 from win32crypt import CryptUnprotectData
 
 
 class ChromeCookieJar(http.cookiejar.CookieJar):
-    'Create CookieJar instances from database of Google Chrome.'
+    'Create CookieJar instance from the Chrome cookies database.'
 
     def __init__(self, cookie_file_path, domain_filter='%', policy=None):
         super().__init__(policy)
@@ -15,37 +16,53 @@ class ChromeCookieJar(http.cookiejar.CookieJar):
         )}
 
         with sqlite3.connect(cookie_file_path) as conn:
-            conn.row_factory = lambda cursor, row: {
-                col[0]: row[idx] for idx, col in enumerate(cursor.description)
-            }
-
-            # Chrome encrypts cookie values since version 33
-            sql = 'pragma table_info("cookies")'
-            encrypted = any(
-                field['name'] == 'encrypted_value'
-                for field in conn.execute(sql)
-            )
-
-            sql = '''
-                select
-                    host_key as domain, name, value, %s
-                    path, expires_utc, is_secure as secure
-                from cookies
-                where host_key like ?
-            ''' % ('encrypted_value,' if encrypted else '')
-
+            conn.row_factory = dict_factory
+            sql_fields = ', '.join(self.get_cookie_fields(conn))
+            sql = 'select %s from cookies where host_key like ?' % sql_fields
             for row in conn.execute(sql, [domain_filter]):
-                row['expires'] = webkit_timestamp_to_unix(row.pop('expires_utc'))
-                if encrypted:
+                row['expires'] = \
+                    webkit_timestamp_to_unix(row.pop('expires_utc'))
+                if row.get('encrypted_value') and not row.get('value'):
                     cipher_blob = row.pop('encrypted_value')
-                    if not row['value']:
-                        row['value'] = \
-                            CryptUnprotectData(cipher_blob)[1].decode()
+                    row['value'] = CryptUnprotectData(cipher_blob)[1].decode()
                 cookie_item = http.cookiejar.Cookie(**row, **dummy, version=0)
                 self.set_cookie(cookie_item)
 
+    @staticmethod
+    def get_cookie_fields(conn: sqlite3.Connection) -> Iterable[str]:
+        'Adjust table fields to be compatible with different Chrome versions.'
+        yield from [
+            'host_key as domain', 'name', 'value', 'path', 'expires_utc'
+        ]
+        fields = set(get_field_names(conn, table='cookies'))
+
+        # Chrome encrypts cookie values since version 33
+        if 'encrypted_value' in fields:
+            yield 'encrypted_value'
+
+        # 'secure' was renamed to 'is_secure' since version 66
+        if 'is_secure' in fields:
+            yield 'is_secure as secure'
+        else:
+            yield 'secure'
+
+
+def dict_factory(cursor, row):
+    'This row factory returns each row as a dict, with field names as the key.'
+    return {
+        col[0]: row[idx]
+        for idx, col in enumerate(cursor.description)
+    }
+
+
+def get_field_names(conn: sqlite3.Connection, table: str) -> Iterable[str]:
+    'Return all field names in the specified database table.'
+    for field in conn.execute('pragma table_info("%s")' % table):
+        yield field['name']
+
 
 def webkit_timestamp_to_unix(webkit_timestamp: int) -> float:
+    'Convert WebKit timestamp to Unix epoch.'
     if not webkit_timestamp:
         return
     t = datetime.datetime(1601, 1, 1)
