08fb2d8fc900cefeb084861fdb4139566f844c89
Arnie97 <arnie97@gmail.com>
2021-04-16 10:13:58 +0800

    Migrate to static metadata as suggested by PyPA

--- setup.py
+++ setup.py
@@ -1,20 +1,2 @@
-import sys
-from setuptools import setup, find_packages
-
-requires = [
-    'pycrypto>=2.6.1'
-]
-
-if sys.version_info < (3, 2):
-    requires.append('futures==2.2')
-
-setup(
-    name='chrome_cookiejar',
-    version='1.0',
-    description=("Python module to load cookies from Google Chrome browser"),
-    url='https://github.com/Arnie97/chrome-cookiejar',
-    author='Arnie97',
-    license='Public domain',
-    packages=find_packages(exclude=['tests']),
-    install_requires=requires
-)
+import setuptools
+setuptools.setup()
